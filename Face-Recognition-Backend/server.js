a33bc4d9174592aaaed45aaba55cf4362f4f1bf3
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-08-23 14:43:02 -0500

    code cleanup

--- Face-Recognition-Backend/server.js
+++ Face-Recognition-Backend/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
 
 const db = knex({
   client: 'pg',
@@ -23,55 +25,9 @@ app.get('/', (req, res) => {
 	res.send(database.users)
 })
 
-app.post('/signin', (req, res) => {
-	db.select('email', 'hash').from('login')
-	.where('email', '=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-			// console.log(isValid)
-			if(isValid){
-				return db.select('*').from('users')
-					.where('email', "=", req.body.email)
-					.then(user => {
-						// console.log(user)
-						res.json(user[0])
-					})
-					.catch(err => res.status(400).json("unable to get user"))
-			} else {
-				res.status(400).json("Wrong email or password")
-			}
-		})
-		.catch(err => res.status(400).json("Wrong email or password"))
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post("/register", (req, res) => {
-	const {email, name, password} = req.body
-	const hash = bcrypt.hashSync(password)
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-			.returning('*')
-			.insert({
-				email: loginEmail[0],
-				name: name,
-				joined: new Date()
-			})
-			.then(user => {
-				res.json(user[0])
-			})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	
-	.catch(err => res.status(400).json('unable to register'))	
-})
+app.post("/register", (req, res) ={register.handleRegister(req, res, db,bcrypt)})
 
 app.get("/profile/:id", (req, res) => {
 	const { id } = req.params
--- Face-Recognition-Frontend/src/App.js
+++ Face-Recognition-Frontend/src/App.js
@@ -26,10 +26,7 @@ const particlesOptions = {
             }
           }
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state = {
+const initialState = {
       input: '',
       imageUrl: '',
       box: {},
@@ -42,6 +39,11 @@ class App extends Component {
               entries: 0,
               joined: ''
       }
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = initialState
     }
   }
 
@@ -101,6 +103,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, {entries:count}))
             })
+            .catch(console.log)
           }
           this.displayFaceBox(this.calculateFaceLocation(response))
         })
@@ -109,7 +112,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
