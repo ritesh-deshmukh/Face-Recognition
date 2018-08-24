56ecb5d2badc8eb01fc6b3f645664cb1846f61d0
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-08-24 12:50:47 -0500

    deploy to heroku

--- Face-Recognition-Backend/controllers/image.js
+++ Face-Recognition-Backend/controllers/image.js
@@ -1,31 +1,31 @@
 const Clarifai = require('clarifai');
 
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '3afa13294f2b4869888e5a0924f4d61c'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
-	app.models
-		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)	
-		.then(data => {
-			res.json(data)
-		})
-		.catch(err => res.status(400).json('Unable to work with api'))
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
-
-
-const handleImage = (db) => (req, res) => {
-	const { id } = req.body
-	db('users').where('id', '=', id)
-	.increment('entries', 1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0])
-	})
-	.catch(err => res.status(400).json("Unable to get entries"))
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
-	handleImage: handleImage
+  handleImage,
+  handleApiCall
 }
